08c86b3ff3b4785a25143f372d409d1b783b7c3a
Jan Safarik <jsafarik@redhat.com>
2023-08-27 08:41:26 +0200

    [Init] Add basic linktree functionality

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import profilePicture from '$lib/profile-picture.jpg';
+</script>
+
+<div>
+	<img src={profilePicture} alt="Profile" />
+	<span><slot /></span>
+</div>
+
+<style lang="scss">
+	$profile-picture-size: 120px;
+
+	div {
+		display: flex;
+		flex-flow: column;
+		align-items: center;
+		margin-top: 20px;
+		font-variation-settings: 'wght' 600;
+
+		span {
+			margin-top: 15px;
+			color: #ddd;
+			text-shadow: 0 2px 4px rgba(0, 0, 0, 0.15);
+		}
+
+		img {
+			border-radius: 100%;
+			width: $profile-picture-size;
+			height: $profile-picture-size;
+			box-shadow: 0 6px 13px -9px rgba(0, 0, 0, 1);
+			object-fit: cover;
+			object-position: center;
+			user-select: none;
+			pointer-events: none;
+		}
+	}
+</style>
---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let url: string;
+</script>
+
+<a href={url}><slot /></a>
+
+<style lang="scss">
+	a {
+		font-size: 15px;
+		padding: 20px 0;
+		text-align: center;
+		background-color: #eee;
+		width: 90%;
+		min-width: 150px;
+		max-width: 500px;
+		font-variation-settings: 'wght' 420;
+		margin: 10px 0;
+		border-radius: 5px;
+		box-shadow: 0 4px 17px -9px rgba(0, 0, 0, 1);
+
+		&:link,
+		&:visited,
+		&:active {
+			color: #222;
+			text-decoration: none;
+		}
+
+		&:hover {
+			color: #111;
+		}
+	}
+</style>
